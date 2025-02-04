5f036fdfd851f101b02ff2f42fe7676b59ae6156
Mabe <mabel.yes@hotmail.com>
2025-02-04 03:06:07 -0500

    Updated search method

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,11 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using magero_store.Models;
 using magero_store.Data;
+using System.Data.SqlClient;
+using Dapper;
 
 namespace magero_store.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public ProductsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public IActionResult Index()
         {
             return View(SampleData.Products);
@@ -19,6 +28,19 @@ namespace magero_store.Controllers
                 return NotFound();
             }
             return View(product);
+        }
+
+        // WARNING: This is deliberately vulnerable to SQL injection!
+        public IActionResult Search(string searchTerm)
+        {
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                // Vulnerable code: Direct string concatenation in SQL query
+                var sql = "SELECT * FROM Products WHERE Name LIKE '%" + searchTerm + "%' OR Description LIKE '%" + searchTerm + "%'";
+                var products = connection.Query<Product>(sql).ToList();
+                return View("Index", products);
+            }
         }
     }
 }
